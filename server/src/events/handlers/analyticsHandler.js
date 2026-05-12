5b71d4b9ba3879ff0de6f39097d554bf510e7f97
Martinez <write2martinez@gmail.com>
2026-05-12 22:34:01 +0100

    fixed analyticshandlers.js

--- server/src/events/handlers/analyticsHandler.js
+++ server/src/events/handlers/analyticsHandler.js
@@ -4,7 +4,7 @@ import { query } from '../../db/pool.js';
 
 const log = createLogger('analyticsHandler');
 
-const log = async (eventName, userId, metadata = {}) => {
+const logEvent = async (eventName, userId, metadata = {}) => {
     try {
         await query(
             `INSERT INTO analytics_events (user_id, event_name, metadata)
@@ -12,42 +12,42 @@ const log = async (eventName, userId, metadata = {}) => {
             [userId ?? null, eventName, JSON.stringify(metadata)]
         );
     } catch (err) {
-        log.error('Failed to log ${eventName}:', { error: err.message });
+        log.error(`Failed to log ${eventName}:`, { error: err.message });
     }
 };
 
 eventBus.on(EVENTS.USER_REGISTERED, ({ userId, email }) =>
-    log('user_registered', userId, { email }));
+    logEvent('user_registered', userId, { email }));
 
 eventBus.on(EVENTS.LESSON_COMPLETED, ({ userId, lessonId, isFirstAttempt }) =>
-    log('lesson_completed', userId, { lessonId, isFirstAttempt }));
+    logEvent('lesson_completed', userId, { lessonId, isFirstAttempt }));
 
 eventBus.on(EVENTS.SUBMISSION_COMPLETED, ({ userId, lessonId, isCorrect, type }) =>
-    log('submission_completed', userId, { lessonId, isCorrect, type }));
+    logEvent('submission_completed', userId, { lessonId, isCorrect, type }));
 
 eventBus.on(EVENTS.QUIZ_ANSWERED, ({ userId, lessonId, isCorrect, attempts }) =>
-    log('quiz_answered', userId, { lessonId, isCorrect, attempts }));
+    logEvent('quiz_answered', userId, { lessonId, isCorrect, attempts }));
 
 eventBus.on(EVENTS.PLAYGROUND_RUN, ({ userId, lessonId }) =>
-    log('playground_run', userId, { lessonId }));
+    logEvent('playground_run', userId, { lessonId }));
 
 eventBus.on(EVENTS.XP_AWARDED, ({ userId, xpEarned, totalXP }) =>
-    log('xp_awarded', userId, { xpEarned, totalXP }));
+    logEvent('xp_awarded', userId, { xpEarned, totalXP }));
 
 eventBus.on(EVENTS.LEVEL_UP, ({ userId, newLevel }) =>
-    log('level_up', userId, { newLevel }));
+    logEvent('level_up', userId, { newLevel }));
 
 eventBus.on(EVENTS.BADGE_EARNED, ({ userId, badgeSlug }) =>
-    log('badge_earned', userId, { badgeSlug }));
+    logEvent('badge_earned', userId, { badgeSlug }));
 
 eventBus.on(EVENTS.STREAK_UPDATED, ({ userId, currentStreak }) =>
-    log('streak_updated', userId, { currentStreak }));
+    logEvent('streak_updated', userId, { currentStreak }));
 
 eventBus.on(EVENTS.STREAK_LOST, ({ userId, previousStreak }) =>
-    log('streak_lost', userId, { previousStreak }));
+    logEvent('streak_lost', userId, { previousStreak }));
 
 eventBus.on(EVENTS.HINT_REQUESTED, ({ userId, lessonId }) =>
-    log('hint_requested', userId, { lessonId }));
+    logEvent('hint_requested', userId, { lessonId }));
 
 eventBus.on(EVENTS.CAREER_STAGE_REACHED, ({ userId, stageSlug }) =>
-    log('career_stage_reached', userId, { stageSlug }));
+    logEvent('career_stage_reached', userId, { stageSlug }));
